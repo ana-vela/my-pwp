5486003d8b8280f6ab9159374e2f45c125d430bc
Ana Vela <avela7@cnm.edu>
2016-10-15 17:36:40 -0600

    added pwp outline

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,5 +1,40 @@
 
 <!DOCTYPE html>
 	<html>
-<p>Testing page</p>
+
+	<head>
+
+		<title>My PWP</title>
+	</head>
+
+	<body>
+
+		<h1>My PWP</h1>
+
+		<section>
+			<h2>Purpose, Audience, and Goals</h2>
+				<h3>Purpose</h3>
+				<p>This project is a website where I can display my web developement work.</p>
+				<h3>Audience</h3>
+				<h3>Goals</h3>
+		</section>
+
+		<section>
+			<h2>Persona</h2>
+
+				<h3>Name</h3>
+				<h3>Age</h3>
+				<h3>Profession</h3>
+				<h3>Technology</h3>
+				<h3>Attitudes and Behaviors</h3>
+				<h3>Frustrations and Needs</h3>
+				<h3>Goals</h3>
+		</section>
+
+		<section>
+			<h2>Use Case</h2>
+		</section>
+
+
+	</body>
 </html>
