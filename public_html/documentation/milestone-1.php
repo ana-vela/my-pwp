3c6f127b0a242669337c2480b5c4937096374a0c
Ana Vela <avela7@cnm.edu>
2016-10-16 15:40:10 -0600

    updated purpose, audience, goals, and persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,28 +11,36 @@
 
 		<h1>My PWP</h1>
 
+		<!-- start purpose, audience, and goals section -->
 		<section>
 			<h2>Purpose, Audience, and Goals</h2>
-				<h3>Purpose</h3>
-				<p>This project is a website where I can display my web developement work.</p>
-				<h3>Audience</h3>
-				<h3>Goals</h3>
+			<ol>
+				<li><strong>Purpose:</strong> This website will be where I can show my web development work to potential employers.</li>
+				<li><strong>Audience:</strong> Potential employers and other web developers who are looking for an intern or junior web developer will be the audience for this website.</li>
+				<li><strong>Goals:</strong> I want to this website to highlight my work so I can get hired for part-time or full-time work as a junior web developer or intern.</li>
+			</ol>
+
 		</section>
 
+		<!-- start persona section -->
 		<section>
 			<h2>Persona</h2>
+				<ul>
+					<li><strong>Name:</strong> Rebecca Bossypants</li>
+					<li><strong>Age:</strong> 35</li>
+					<li><strong>Profession:</strong> Rebecca, founder of ABQ Web Development, is looking to expand her staff by hiring a few interns so they can take on more projects.</li>
+					<li><strong>Technology:</strong> Rebecca and her staff use Windows for all their projects and most have Android phones.</li>
+				<li><strong>Attitudes and Behaviors:</strong> With all her responsibilities, Rebecca will quickly look through portfolio sites and profiles on LinkedIn in hopes of finding the best candidates of interns.</li>
+				<li><strong>Frustrations and Needs:</strong> Rebecca need interns who are familiar with Angular 2 and are willing get further training with the various programs and procedures adopted in her web group.</li>
+				<li><strong>Goals:</strong> Because she is short on time, she will scan the portfolio sites to see if potential candidates have the experience she is looking for.</li>
+			</ul>
 
-				<h3>Name</h3>
-				<h3>Age</h3>
-				<h3>Profession</h3>
-				<h3>Technology</h3>
-				<h3>Attitudes and Behaviors</h3>
-				<h3>Frustrations and Needs</h3>
-				<h3>Goals</h3>
 		</section>
 
+		<!-- start use case section -->
 		<section>
 			<h2>Use Case</h2>
+
 		</section>
 
 
