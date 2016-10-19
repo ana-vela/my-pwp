e335796f137e1aa317306fd4a752a3ebd1df63b6
Ana Vela <avela7@cnm.edu>
2016-10-19 12:54:37 -0600

    made changes to technology and use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -15,9 +15,9 @@
 		<section>
 			<h2>Purpose, Audience, and Goals</h2>
 			<ol>
-				<li><strong>Purpose:</strong> This website will be where I can show my web development work to potential employers.</li>
-				<li><strong>Audience:</strong> Potential employers and other web developers who are looking for an intern or junior web developer will be the audience for this website.</li>
-				<li><strong>Goals:</strong> I want to this website to highlight my work so I can get hired for part-time or full-time work as a junior web developer or intern.</li>
+				<li><strong>Purpose:</strong> This website will be where I display my web development portfolio and contact information.</li>
+				<li><strong>Audience:</strong> Potential employers and other web developers who are looking for an intern or junior web developer as well as clients looking for a freelance web developer will be the audience for this website.</li>
+				<li><strong>Goals:</strong> I want to this website to highlight my work so I can get hired for part-time or full-time work as a junior web developer or intern as well as freelance jobs.</li>
 			</ol>
 
 		</section>
@@ -26,13 +26,13 @@
 		<section>
 			<h2>Persona</h2>
 				<ul>
-					<li><strong>Name:</strong> Rebecca Bossypants</li>
+					<li><strong>Name:</strong> Christina Bossypants</li>
 					<li><strong>Age:</strong> 35</li>
-					<li><strong>Profession:</strong> Rebecca, founder of ABQ Web Development, is looking to expand her staff by hiring a few interns so they can take on more projects.</li>
-					<li><strong>Technology:</strong> Rebecca and her staff use Windows for all their projects and most have Android phones.</li>
-				<li><strong>Attitudes and Behaviors:</strong> With all her responsibilities, Rebecca will quickly look through portfolio sites and profiles on LinkedIn in hopes of finding the best candidates of interns.</li>
-				<li><strong>Frustrations and Needs:</strong> Rebecca need interns who are familiar with Angular 2 and are willing get further training with the various programs and procedures adopted in her web group.</li>
-				<li><strong>Goals:</strong> Because she is short on time, she will scan the portfolio sites to see if potential candidates have the experience she is looking for.</li>
+					<li><strong>Profession:</strong> Christina, founder of ABQ Web Development, is looking to expand her staff by hiring a few interns so the company can take on more projects.</li>
+					<li><strong>Technology:</strong> Christina and her staff use Windows 10 for all their projects and most have Android phones.</li>
+				<li><strong>Attitudes and Behaviors:</strong> Christina will spend a few minutes to look through portfolio sites of applicants in hopes of finding the best candidates of interns.</li>
+				<li><strong>Frustrations and Needs:</strong> Christina needs interns who are familiar with Angular 2 and are willing get further training with the various programs and procedures adopted in her web group.</li>
+				<li><strong>Goals:</strong> Because she is short on time, she will scan the portfolio sites of the top 10 potential candidates who have the experience she is looking for.</li>
 			</ul>
 
 		</section>
@@ -40,6 +40,8 @@
 		<!-- start use case section -->
 		<section>
 			<h2>Use Case</h2>
+				<p>Christina advertised for two interns on a college job board and asked for potential candidates to include a resume and cover letter with a portfolio link. After one week, Christina looks at the applications so far. Because there are so many applicants, she chooses the top 10 candidates and browses through their portfolio sites. Christina sees that I am one of the few applicants with Angular 2 experience. She also recognizes my name from a networking event at FatPipe ABQ. After looking through my portfolio site, Christina decides to call me that day to set up an interview for Tuesday morning of the following week.
+				</p>
 
 		</section>
 
