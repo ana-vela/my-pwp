3279b9b3e13fcf9480e97e6492c16284b8548188
Ana Vela <avela7@cnm.edu>
2016-10-23 15:22:54 -0600

    started milestone-2 document and added wireframes

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		 <!-- title of assignment here -->
+			<title>Milestone 2 Documentation</title>
+	</head>
+
+	<body>
+		<h1>Milestone 2</h1>
+
+		<img src="../img/anavela-desktop-wireframe.png" alt="desktop wireframe">
+		<img src="../img/anavela-mobile-wireframe.png" alt="mobile wireframe">
+
+	</body>
+	</html>
