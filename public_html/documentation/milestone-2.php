54c37bfc9ef46f13294569ec0271a9ae9ccbab0b
Ana Vela <avela7@cnm.edu>
2016-10-27 21:30:24 -0600

    added basic headings to page

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,10 +7,29 @@
 	</head>
 
 	<body>
-		<h1>Milestone 2</h1>
+		<h1>My PWP - Milestone 2a</h1>
 
-		<img src="../img/anavela-desktop.png" alt="desktop wireframe">
-		<img src="../img/anavela-mobile-wireframe.png" alt="mobile wireframe">
+		<!--content strategy starts here -->
+			<section>
+				<h2>Content Strategy</h2>
+
+					<ul>
+						<li>Header:</li>
+						<li>About Me:</li>
+						<li>Portfolio:</li>
+						<li>Contact:</li>
+
+					</ul>
+			</section>
+
+		<!--wireframes start here -->
+			<section>
+				<h2>Full Screen Layout</h2>
+					<img src="../img/anavela-desktop.png" alt="desktop wireframe">
+
+				<h2>Mobile Layout</h2>
+					<img src="../img/anavela-mobile-wireframe.png" alt="mobile wireframe">
+			</section>
 
 	</body>
 	</html>
