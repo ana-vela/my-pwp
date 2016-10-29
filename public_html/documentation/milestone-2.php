9bbc21117c344c8d8c035da5cfe49d2b13428b2e
Ana Vela <avela7@cnm.edu>
2016-10-29 14:48:08 -0600

    added descriptions to content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -14,11 +14,10 @@
 				<h2>Content Strategy</h2>
 
 					<ul>
-						<li>Header:</li>
-						<li>About Me:</li>
-						<li>Portfolio:</li>
-						<li>Contact:</li>
-
+						<li>Header: My name and title will be in the center of the header. There will be a navigation bar below the header where visitors can easily locate the other categories on my site.</li>
+						<li>About Me: I will describe my path to web development and a little more about myself. I will include my strengths and personality traits to emphasis how I could be a compatible team member. There will be a table where I list the technical skills I've gained during DDCB.</li>
+						<li>Portfolio: This will be the image gallery for my projects. I will list  a short description of each project, what frameworks and programming languages I used, and how I worked on it (i.e. capstone group project or freelance work with other developers).</li>
+						<li>Contact: Those interested in working with me can send me a message through the website. This will be a simple form with the essential fields to fill out. The footer will have icons directing visitors to my other profiles on the web. Next to the contact form, I will invite them to contact me for contract work or positions at their organization.</li>
 					</ul>
 			</section>
 
@@ -28,8 +27,9 @@
 					<img src="../img/anavela-desktop.png" alt="desktop wireframe">
 
 				<h2>Mobile Layout</h2>
+				<p>These are approximations. With the help of Bootstrap, the features will be adjusted based on screen size.</p>
 					<img src="../img/anavela-mobile-wireframe.png" alt="mobile wireframe">
 			</section>
 
-	</body>
+		</body>
 	</html>
